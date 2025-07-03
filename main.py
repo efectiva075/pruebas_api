13855e953498a90e85424c3524ac2eaee2f74e98
efectiva075 <wfigueroa@efectiva.com.pe>
2025-07-03 17:09:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,20 @@ from datetime import datetime
 from fastapi import FastAPI
 from pydantic import BaseModel
 import os
+import logging
 from dotenv import load_dotenv
 load_dotenv()
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("api.log"),       # Log en archivo
+        logging.StreamHandler()               # Log en consola
+    ]
+)
+
+logger = logging.getLogger(__name__)
 
 class User(BaseModel):
     id: int
@@ -16,10 +28,12 @@ app = FastAPI()
 
 @app.get("/")
 def main():
+    logger.info("GET/llamado"))
     return {"mesage":f"Hora actual: {str(datetime.now())}",
            "clave":os.getenv('clave','no encontro'),
            "msj":os.getenv('msj','no hay mensaje')}
 
 @app.post("/user")
 def user(user:User):
+    logger.info(f"POST /user con datos: {user}")
     return user
