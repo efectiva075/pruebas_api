1f37f69285f314bc8516072636e23d9351f7c167
efectiva075 <wfigueroa@efectiva.com.pe>
2025-07-03 15:18:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
+# from datetime import datetime
+# print("►►►HORA ACTUAL:",str(datetime.now()))
+
 from datetime import datetime
-print("►►►HORA ACTUAL:",str(datetime.now()))
+from fastapi import FastAPI
+from pydantic import BaseModel
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class User(BaseModel):
+    id: int
+    name: str
+
+app = FastAPI()
+
+@app.get("/")
+def main():
+    return {"mesage":f"Hora actual: {str(datetime.now())}",
+           "clave":os.getenv('clave','no encontro')}
+
+@app.post("/user")
+def user(user:User):
+    return user
